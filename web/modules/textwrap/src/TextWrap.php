f34c006b3ea0f62a0fd59e7c5ed68f59a7024c33
Carlos Eduardo <cadudevmaster@gmail.com>
2021-06-10 18:59:42 -0300

    Implementado função que divide string em array

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -19,14 +19,21 @@ class TextWrap implements TextWrapInterface {
   /**
    * {@inheritdoc}
    */
-  private function string_split_unicode(){
-
+  private function string_split(string $str):array{
+    if (!empty($str)) {
+        $newArray = array();
+        $size = mb_strlen($str, "UTF-8");
+        for ($i = 0; $i < $size; $i += $l) {
+            $newArray[] = mb_substr($str, $i, $l, "UTF-8");
+        }
+        return $newArray;
+    }
   }
-  private function IdentifierBigWords(array $words,int $length){
+  private function IdentifierBigWords(array $words,int $length):array{
     
   }
   public function wrap(string $text, int $length): array {
-      //Aqui verifico que o texto passado está vazio
+      //Aqui verifico se o texto passado está vazio
       if(!empty($text)){
           $array=[];
       }
