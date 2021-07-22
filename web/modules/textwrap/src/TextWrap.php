fb9bbb83eb7367761505200f70aa4d92b4e46f38
svhenrique <silviohenrique1234@hotmail.com>
2021-07-21 22:16:55 -0300

    adicionado funçao split

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -56,6 +56,33 @@ class TextWrap implements TextWrapInterface {
     return $text_result;  
   }
   
+  private function split(string $text): array {
+    // Função para dividir string em array de acordo com espaços em branco
+    // exemplo "sobre o ombro de gigantess" ficaria 
+    // ["sobre", "o", "ombro", "de", "gigantes"]
+
+    $text_result = [];
+    $step = 0;
+    $start = 0;
+    $len_text = mb_strlen($text, "utf-8"); 
+
+    while ($step < $len_text) {  
+      // Se encontrar um espaço em branco ou estiver na ultima palavra
+      if (($text[$step] === " ") or ($step === $len_text-1)) {
+        $end = $step;
+        
+        // se estiver na ultima letra da ultima palavra
+        if ($step === $len_text-1) {
+          $end ++;
+        }
+
+        $text_result[] = substr($text, $start, $end-$start);
+        $start = $step + 1;
+      } 
+      $step ++;
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -63,44 +90,5 @@ class TextWrap implements TextWrapInterface {
     // Apague o código abaixo e escreva sua própria implementação,
     // nós colocamos esse mock para poder rodar a análise de cobertura dos
     // testes unitários.
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
-
-    return [""];
-  }
-
+    return [];
 }
-
