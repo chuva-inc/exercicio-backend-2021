6be53614b898ff106e77fdb0c923ed88f02b17bf
Maths Physis <smathsphysis@gmail.com>
2021-07-21 22:21:05 -0400

    refactor: Changed testForSmallWords3() to be more elegant

--- web/modules/textwrap/tests/src/Unit/TextWrapTest.php
+++ web/modules/textwrap/tests/src/Unit/TextWrapTest.php
@@ -32,7 +32,7 @@ class TextWrapTest extends TestCase
         $this->assertCount(1, $ret);
     }
 
-    public function testForEmptylength()
+    public function testForEmptyLength()
     {
         $ret = $this->resolucao->wrap($this->baseString, 0);
         $this->assertEmpty($ret[0]);
@@ -77,35 +77,19 @@ class TextWrapTest extends TestCase
      */
     public function testForSmallWords3()
     {
-        $ret = $this->resolucao->wrap($this->baseString, 2);
-        $this->assertEquals("Se", $ret[0]);
-        $this->assertEquals("vi", $ret[1]);
-        $this->assertEquals("ma", $ret[2]);
-        $this->assertEquals("is", $ret[3]);
-        $this->assertEquals("lo", $ret[4]);
-        $this->assertEquals("ng", $ret[5]);
-        $this->assertEquals("e", $ret[6]);
-        $this->assertEquals("fo", $ret[7]);
-        $this->assertEquals("i", $ret[8]);
-        $this->assertEquals("po", $ret[9]);
-        $this->assertEquals("r", $ret[10]);
-        $this->assertEquals("es", $ret[11]);
-        $this->assertEquals("ta", $ret[12]);
-        $this->assertEquals("r", $ret[13]);
-        $this->assertEquals("de", $ret[14]);
-        $this->assertEquals("pé", $ret[15]);
-        $this->assertEquals("so", $ret[16]);
-        $this->assertEquals("br", $ret[17]);
-        $this->assertEquals("e", $ret[18]);
-        $this->assertEquals("om", $ret[19]);
-        $this->assertEquals("br", $ret[20]);
-        $this->assertEquals("os", $ret[21]);
-        $this->assertEquals("de", $ret[22]);
-        $this->assertEquals("gi", $ret[23]);
-        $this->assertEquals("ga", $ret[24]);
-        $this->assertEquals("nt", $ret[25]);
-        $this->assertEquals("es", $ret[26]);
-        $this->assertCount(27, $ret);
+        $ret = $this->resolucao->wrap($this->baseString, 7);
+        $this->assertEquals("Se vi", $ret[0]);
+        $this->assertEquals("mais", $ret[1]);
+        $this->assertEquals("longe", $ret[2]);
+        $this->assertEquals("foi por", $ret[3]);
+        $this->assertEquals("estar", $ret[4]);
+        $this->assertEquals("de pé", $ret[5]);
+        $this->assertEquals("sobre", $ret[6]);
+        $this->assertEquals("ombros", $ret[7]);
+        $this->assertEquals("de", $ret[8]);
+        $this->assertEquals("gigante", $ret[9]);
+        $this->assertEquals("s", $ret[10]);
+        $this->assertCount(11, $ret);
     }
 
 }
