836dcc9a69fc911e9b37d1e45347d8a32c9db1a4
svhenrique <silviohenrique1234@hotmail.com>
2021-07-22 15:45:59 -0300

    adicionado funçao de teste com foco em corte de palavras

--- web/modules/textwrap/tests/src/Unit/TextWrapTest.php
+++ web/modules/textwrap/tests/src/Unit/TextWrapTest.php
@@ -20,6 +20,7 @@ class TextWrapTest extends TestCase {
     $this->baseString = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
     $this->stripString = "   Se vi mais longe foi por estar de pé sobre ombros de gigantes     ";
     $this->inStripString = "Se  vi   mais   longe    foi por estar  de pé   sobre   ombros   de    gigantes";
+    $this->testCutString = "Teste para corte";
   }
 
   /**
@@ -97,7 +98,6 @@ class TextWrapTest extends TestCase {
     $this->assertEquals("gigantes", $ret[8]);
     $this->assertCount(9, $ret);
   }
-
   
   /**
    * Testa a função quando length for 0.
@@ -105,5 +105,21 @@ class TextWrapTest extends TestCase {
   public function testForZeroLength() {
     $ret = $this->resolucao->wrap($this->inStripString, 0);
     $this->assertEmpty($ret);
+  }
+
+  /**
+   * Testa a função quebra de linha quando length é pequeno
+   * e será necessário cortar palavras.
+   */
+  public function testForCutString() {
+    $ret = $this->resolucao->wrap($this->testCutString, 3);
+    $this->assertEquals("Tes", $ret[0]);
+    $this->assertEquals("te", $ret[1]);
+    $this->assertEquals("par", $ret[2]);
+    $this->assertEquals("a", $ret[3]);
+    $this->assertEquals("cor", $ret[4]);
+    $this->assertEquals("te", $ret[5]);
+    $this->assertCount(6, $ret);
+
   }
 }
