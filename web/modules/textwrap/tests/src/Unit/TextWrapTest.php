a33b4e39152c5af2e112d08fdb23c37d7dccf7db
Joao Nascimento <joao.nascimento0576@gmail.com>
2021-08-01 12:40:52 -0300

    Adição de mais alguns cenários de teste

--- web/modules/textwrap/tests/src/Unit/TextWrapTest.php
+++ web/modules/textwrap/tests/src/Unit/TextWrapTest.php
@@ -60,4 +60,48 @@ class TextWrapTest extends TestCase {
     $this->assertCount(6, $ret);
   }
 
+  // Testa a quebra de linha para um limite de 5 caracteres
+  public function testForSmallWords3() {
+    $ret = $this->resolucao->wrap($this->baseString, 5);
+    $this->assertEquals("Se vi", $ret[0]);
+    $this->assertEquals("mais", $ret[1]);
+    $this->assertEquals("longe", $ret[2]);
+    $this->assertEquals("foi", $ret[3]);
+    $this->assertEquals("por", $ret[4]);
+    $this->assertEquals("estar", $ret[5]);
+    $this->assertEquals("de pé", $ret[6]);
+    $this->assertEquals("sobre", $ret[7]);
+    $this->assertEquals("ombro", $ret[8]);
+    $this->assertEquals("s de", $ret[9]);
+    $this->assertEquals("gigan", $ret[10]);
+    $this->assertEquals("tes", $ret[11]);
+    $this->assertCount(12, $ret);
+  }
+
+  // Testa a quebra de linha para um limite de 3 caracteres
+  public function testForSmallWords4() {
+    $ret = $this->resolucao->wrap($this->baseString, 3);
+    $this->assertEquals("Se", $ret[0]);
+    $this->assertEquals("vi", $ret[1]);
+    $this->assertEquals("mai", $ret[2]);
+    $this->assertEquals("s", $ret[3]);
+    $this->assertEquals("lon", $ret[4]);
+    $this->assertEquals("ge", $ret[5]);
+    $this->assertEquals("foi", $ret[6]);
+    $this->assertEquals("por", $ret[7]);
+    $this->assertEquals("est", $ret[8]);
+    $this->assertEquals("ar", $ret[9]);
+    $this->assertEquals("de", $ret[10]);
+    $this->assertEquals("pé", $ret[11]);
+    $this->assertEquals("sob", $ret[12]);
+    $this->assertEquals("re", $ret[13]);
+    $this->assertEquals("omb", $ret[14]);
+    $this->assertEquals("ros", $ret[15]);
+    $this->assertEquals("de", $ret[16]);
+    $this->assertEquals("gig", $ret[17]);
+    $this->assertEquals("ant", $ret[18]);
+    $this->assertEquals("es", $ret[19]);
+    $this->assertCount(20, $ret);
+  }
+
 }
