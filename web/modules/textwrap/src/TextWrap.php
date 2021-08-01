7be6e399ab047e9c092cfa8d399b25d7ecd95613
Joao Nascimento <joao.nascimento0576@gmail.com>
2021-08-01 12:21:17 -0300

    Adição de validações para validar a string de input

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -20,45 +20,45 @@ class TextWrap implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function wrap(string $text, int $length): array {
-    // Apague o código abaixo e escreva sua própria implementação,
-    // nós colocamos esse mock para poder rodar a análise de cobertura dos
-    // testes unitários.
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
+    
+    if ($length == 0) { return [""]; }
 
+    $parts = slash($text);
+
+    if (!isset($parts)) { return [""]; }
+
+    // A resolução vai aqui abaixo :)
+    $parts = orderParts($parts, $length);
+
+    return $parts;
+  }
+
+  public function slash(string $text) {
+    // verifica se o texto informado está vazio
+    if (mb_strlen($text, "utf-8") == 0) { return null; }
+    // correção de espaços duplicados/repetidos
+    $text = replaceWhiteSpaces($text);
+    // retorna as palavras da string text ordenadas por indices
+    // em um array.
+    return explode(" ", $text);
+  }
+
+  // remove espaços duplicados ou repetidos entre as palavras.
+  public function replaceWhiteSpaces($str) {
+      // verifica e corrige através de uma expressão regex os espaços 
+      // repetidos da string e a retorna.
+      return ($str) ? trim(preg_replace('/\s+/', ' ',$str)) : $str;
+  }
+
+
+/* 
+ * setNewPartsLength
+ * 
+ * ordena os índices do array colocando o máximo
+ * de palavras por linha, considerando a quantidade de 
+ * caracteres máxima espeficada na variável length
+*/
+  public function setNewPartsLength(array $parts, int $length) {
     return [""];
   }
 
