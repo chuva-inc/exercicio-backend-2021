88111054fee58d92e35059ca415c91b74a248362
Joao Nascimento <joao.nascimento0576@gmail.com>
2021-08-01 12:49:07 -0300

    Adição de corpo da função setNewPartsLength

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -59,7 +59,67 @@ class TextWrap implements TextWrapInterface {
  * caracteres máxima espeficada na variável length
 */
   public function setNewPartsLength(array $parts, int $length) {
-    return [""];
+    // definindo a codificação de caracteres a ser usada na função
+    // mb_strlen
+    $encoding = "utf-8";
+    // variável temporária para armazenar as novas strings.
+    $tempString = "";
+    // novo array de strings
+    $newParts = [];
+    $newPartsIndex = 0;
+
+    // variável usada na condição envolvendo quebra de palavras
+    // maiores que a variável length
+    $startIndex = 0;    
+
+    for ($i = 0; $i < count($parts); $i++):
+
+        if(mb_strlen($parts[$i], $encoding) < $length || mb_strlen($tempString, $encoding) > 0)  {
+            // atribui um valor a variável tempstring, se ela estiver nula.
+            if (mb_strlen($tempString, $encoding) == 0) {
+                $tempString = $parts[$i];
+            }
+            // verifica se é possível concartenar mais um valor à variável tempString
+            // sem que ela estoure o limite de caracteres
+            elseif (mb_strlen(($tempString . " " . $parts[$i]), $encoding) <= $length) {
+                $tempString .= " " . $parts[$i];
+            }
+            // verifica se o valor de tempString estourará o limite de caracteres ou já
+            // possui um valor idêntico ao definido em length
+            elseif (mb_strlen(($tempString . " " . $parts[$i]), $encoding) > $length || mb_strlen($tempString, $encoding) == $length) {
+                $newParts[$newPartsIndex] = $tempString;
+                $newPartsIndex++;
+                $tempString = "";
+                $i--;
+            }
+        }
+        // palavras de tamanho igual ao de length
+        elseif (mb_strlen($parts[$i], $encoding) == $length) {
+            $newParts[$newPartsIndex] = $parts[$i];
+            $newPartsIndex++;
+        }
+        // palavras maiores que length
+        else {
+
+            if (mb_strlen($tempString, $encoding) == 0) {
+                $tempString = substr($parts[$i], $startIndex, $length);
+                $startIndex += $length; 
+
+                if ($tempString == null) { $startIndex = 0; }
+                else { 
+                    $i--; 
+                }
+            }
+            // verifica se a substring já tem um valor para ser incrementado ao array
+            elseif (mb_strlen($tempString, $encoding) == $length) {
+                $newParts[$newPartsIndex] = $tempString;
+                $tempString = "";
+            }
+
+        }    
+    endfor;
+
+    return $newParts;
   }
 
 }
