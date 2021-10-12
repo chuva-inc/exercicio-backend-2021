c8f4e2a608013a62401f58932703500c437549f7
Kuticoski <guirkromanini01@gmail.com>
2021-10-12 15:36:28 -0400

    Refatora o código.
    Aumenta a legibilidade do código e reduz sua complexidade

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -15,47 +15,49 @@ class TextWrap implements TextWrapInterface {
     }
 
     $result = array();
-
-    $lastPos = mb_strlen($text)-1;
     $currentWord = "";
     $currentLine = "";
     // Percorre a String $text
-    for($pos = 0; $pos <= $lastPos; $pos++){
-      // Pega o caracter atual
+    for($pos = 0; $pos < mb_strlen($text); $pos++){
+      // Pega o caractere atual
       $char = mb_substr($text, $pos, 1);
 
-      // Caso o caractere atual seja um espaço vazio, indica o fim de uma palavra
-      // e o começo da próxima
+      // Um espaço vazio indica o fim de uma palavra e o começo da próxima
       if($char === ' '){
-        // Se a última palavra couber na linha ela é inserida, 
-        // caso não, a linha escrita até então é inserida no array $result
-        // e a palvra é colocada na próxima linha
-        if(mb_strlen($currentWord) + mb_strlen($currentLine) >= $length) {
+        /** 
+         * Verifica se cabe mais uma palavra na linha
+         * se não couber insere a linha no array e coloca a palavra na próxima linha
+         */
+        if(mb_strlen($currentWord)+1 + mb_strlen($currentLine) > $length) {
           array_push($result, $currentLine);
-          $currentLine = $currentWord;
-        }  else if (mb_strlen($currentLine) === 0) {
-          // Na primeira palavra de cada linha não é colocado um espaço em branco antes
-          $currentLine .= $currentWord; 
-        } else {
-          $currentLine .= " " . $currentWord; 
+          $currentLine = "";
         }
+
+        // Adiciona-se um espaço vazio entre as palavras, exceto se for a primeira da linha
+        if (mb_strlen($currentLine) !== 0) {
+          $currentLine .= " "; 
+        }
+
+        // Adiciona a palavra na linha e coloca $currentWord como vazio 
+        $currentLine .= $currentWord; 
         $currentWord = "";
-      } else if($pos === $lastPos) {
-        $currentWord .= $char;
-        // Se a última palavra do texto não couber na linha atual
-        // a palavra é inserida na próxima posição do array.
-        if(mb_strlen($currentWord) + mb_strlen($currentLine) >= $length) {
-          array_push($result, $currentLine);
-          array_push($result, $currentWord);
-        } else {
-          $currentLine .= $currentWord;
-          array_push($result, $currentLine);
-        }
       } else {
-        // O atual caractere será inserido na palavra atual, 
-        // caso não seja um espaço em branco.
         $currentWord .= $char;
       }
+    }
+
+    // Adiciona as linhas finais a $result
+
+    /**
+     * Se a última palavra não couber na linha, então $currentLine será inserido
+     * primeiro e $currentWord virá depois, em uma linha separas
+     */ 
+    if(mb_strlen($currentWord)+1 + mb_strlen($currentLine) > $length) {
+      array_push($result, $currentLine);
+      array_push($result, $currentWord);
+    } else {
+      $currentLine .= " " . $currentWord;
+      array_push($result, $currentLine);
     }
 
     return $result;
